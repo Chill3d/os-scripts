762769b1b077abaf756f9ab346edcfb78df34285
Chill3d <chill3d@protonmail.com>
2018-08-29 14:42:54 +0200

    Old folder creation and fix URL

--- kali.sh
+++ kali.sh
@@ -1,3 +1,3 @@
-    Kali 1.x: https://github.com/g0tmi1k/os-scripts/blob/master/kali1.sh
-    Kali 2.x: https://github.com/g0tmi1k/os-scripts/blob/master/kali2.sh
-Kali-Rolling: https://github.com/g0tmi1k/os-scripts/blob/master/kali-rolling.sh
+    Kali 1.x: https://github.com/Chill3d/os-scripts/blob/master/old/kali1.sh
+    Kali 2.x: https://github.com/Chill3d/os-scripts/blob/master/old/kali2.sh
+Kali-Rolling: https://github.com/Chill3d/os-scripts/blob/master/kali-rolling.sh
